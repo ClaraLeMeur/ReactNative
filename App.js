9d4ee7d77b256555a7eead1fc59e44400df427be
Clara <clara.lemeur@talkme.tech>
2020-12-14 11:25:25 +0100

    v1

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.containerView1}>
+			<Text>Ma Première application React Native</Text>
+			<Text>Je m'appel : Clara Le Meur</Text>
+			<Text>Et on ne demande pas leur age au dame!</Text>
+			<Text>Je fait partie des RIL 2020</Text>
+			<Image source={require('./assets/icon.png')} style={{ width: 100, height: 100 }} />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	containerScroolView: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
+	containerView1: {
+		flex: 1,
+		backgroundColor: '#fff',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	containerView2: {
+		display: 'flex',
+		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		backgroundColor: '#E5E5E5',
+	},
 });
